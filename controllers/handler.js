bf0e2190cce8d4668740e3ffc7b3de8fbb7c59cf
ravenjohn <rjlagrimas08@gmail.com>
2014-06-06 21:22:33 +0800

    Started nearest establishment

--- controllers/handler.js
+++ controllers/handler.js
@@ -3,15 +3,18 @@ var config = require(__dirname + '/../config/config'),
 	curl = require(__dirname + '/../lib/curl');
 
 exports.handle_sms = function (req, res, next) {
-	var get_access_token = function (status, result) {
+	var data,
+		get_access_token = function (status, result) {
 			if (status !== 200) return next(result);
+			data = result;
 			mongo.collection('users')
 				.insert(result, send_response)
 		},
 		send_response = function (err) {
 			if (err) return next(err);
-			res.redirect(config.frontend_url + 'success');
+			res.redirect(config.frontend_url + '#/success');
 		};
+
 	curl.post
 		.to('developer.globelabs.com.ph', 80, '/oauth/access_token')
 		.send({
@@ -21,4 +24,40 @@ exports.handle_sms = function (req, res, next) {
 		})
 		.then(get_access_token)
 		.onerror(next);
+};
+
+exports.get_establishments = function (req, res, next) {
+};
+
+exports.get_nearest_establishment = function (req, res, next) {
+	var data = data.inboundSMSMessageList.inboundSMSMessage[0],
+		sender = data.senderAddress,
+		message = data.message,
+		get_access_token = function (err, result) {
+			if (err) return next(err);
+			curl.get
+				.to('devapi.globelabs.com.ph', 80, '/location/v1/queries/location')
+				.send({
+					access_token : result.access_token,
+					address : '+63' + result.subscriber_number,
+					requestedAccuracy : 100
+				})
+				.then(get_nearest_establishment)
+				.onerror(next);
+		},
+		get_nearest_establishment = function (status, result) {
+			if (status !== 200) return next(result);
+			mongo.collection('establishments')
+				.find({
+					subscriber_number : sender
+				}, send_response)
+		},
+		send_response = function (err, result) {
+			if (err) return next(err);
+			if (result === 0)
+			res.redirect(config.frontend_url + '#/success');
+		};
+
+	mongo.collection('users')
+		.findOne({subscriber_number : sender}, get_access_token)
 };
