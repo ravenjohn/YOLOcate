222ac8253c2e0e919a0d88ab3b526e24d67f87a0
ravenjohn <rjlagrimas08@gmail.com>
2014-06-07 09:16:20 +0800

    used 1 allowed server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(require('cookie-parser')(config.cookie_secret));
 app.use(require('response-time')());
 app.use(require('compression')());
 app.use(require('method-override')());
-app.use(require(__dirname + '/lib/cors')(config.allowed_servers.join(',')));
+app.use(require(__dirname + '/lib/cors')(config.allowed_server));
 app.use(require(__dirname + '/config/router')(express.Router(), logger));
 
 app.listen(config.port);
